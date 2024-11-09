89329f1dc384d861b7e303e7f342f0f6ee3ccc0b
Navid M <navidmomtahen@gmail.com>
2024-11-09 12:14:00 +0000

    add checks

---
+++ src/mods/checks.nim
@@ -0,0 +1,16 @@
+import
+    std/strutils,
+    filters
+
+
+proc run_rec_error_test*() =
+    var to_check_for = "fox"
+
+    when defined windows:
+        to_check_for = to_check_for & ".exe"
+
+    if get_executable_name() == to_check_for:
+        echo("Recursion issues may arise due to identical binary names, proceed anyway? (Y/N)")
+        var confirmation = toLowerAscii(stdin.read_line())
+        if (confirmation != "y"):
+            quit(0)
